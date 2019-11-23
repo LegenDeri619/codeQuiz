6cbbe4530784721599d9f42422d093d85800e57d
Aidesiderio Alhambra <deri.alhambra@gmail.com>
2019-11-23 14:33:35 -0800

    html and css framework made

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,26 +45,7 @@ $(document).ready(function() {
 
 //function for global timer
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.textContent = minutes + ":" + seconds;
-
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-window.onload = function () {
-    var fiveMinutes = 60 * 1,
-        display = document.querySelector('#time');
-    startTimer(oneMinutes, display);
+    
 };
 //time bar = 100% = green bar
 //render each second red bar: (elapsed time/total time) * 100%
